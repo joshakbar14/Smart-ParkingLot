b693feee1c3dc20b77806d030a85cfa566d1cfb4
Your Name <jakabar14@outlook.com>
2023-03-06 00:38:25 +0000

    gpioSetAlert function

--- src/os/ultrasonicSensorClass.cpp
+++ src/os/ultrasonicSensorClass.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <ctime>
 #include "unistd.h"
@@ -27,9 +28,9 @@ ultrasonicSensorClass::ultrasonicSensorClass(int in, int out, int sensor_no)
     // this->gpioRead.export_gpio(); // initialize
     // this->gpioRead.setdir_gpio("in");
 
-    /* 
+    /*
      * Re-create the whole GPIOClass using PIGPIO library
-     * For common language and easy-to-use library 
+     * For common language and easy-to-use library
      */
     input_pin = in;
     output_pin = out;
@@ -46,31 +47,40 @@ int ultrasonicSensorClass::sense_location()
     try
     {
         while (true)
-        {	    
+        {
             gpioWrite(output_pin, 1); // trigger high
             usleep(100);
             gpioWrite(output_pin, 0); // trigger low
             // save start time
-            
-            while (gpioRead(input_pin) == 1)
+
+            // while (gpioRead(input_pin) == 1)
+            // {
+            //     // get GPIO read result
+            //     startTime = time(&startTime);
+            // }
+
+            // // save stop time
+
+            // while (gpioRead(input_pin) == 0)
+            // {
+            //     stopTime = time(&stopTime);
+            // }
+
+            void aFunction(int gpio, int level, uint32_t tick)
             {
-                // get GPIO read result
-                startTime = time(&startTime);
+                printf("GPIO %d became %d at %d", gpio, level, tick);
             }
 
-            // save stop time
-            
-            while (gpioRead(input_pin) == 0)
-            {
-                stopTime = time(&stopTime);
-            }
+            // call aFunction whenever GPIO 4 changes state
+
+            gpioSetAlertFunc(4, aFunction);
 
             // main calculations, speed of sound
-            int timeElapsed = startTime - stopTime;
-            int distance = (timeElapsed / 58.2);
-            cout << distance << endl;
-            cout << startTime << endl;
-	    cout << stopTime << endl;
+            // int timeElapsed = startTime - stopTime;
+            // int distance = (timeElapsed / 58.2);
+            // cout << distance << endl;
+            // cout << startTime << endl;
+            // cout << stopTime << endl;
             sleep(1);
         }
     }
